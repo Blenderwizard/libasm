bcd28b15ea25706deb0954279e9916642e13edf4
Jolan <rathelotj@gmail.com>
2022-09-25 15:27:37 +0200

    began work on ft_strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,3 +2,10 @@ section .text
 global _ft_strcmp
 
 _ft_strcmp:
+	PUSH	RBP ; establish a stack frame
+    MOV		RBP, RSP
+
+	; PUSH	
+	; CALL	_ft_strlen ; calls 
+	LEAVE ; Tears down the stack frame
+	RET ; Returns RAX
