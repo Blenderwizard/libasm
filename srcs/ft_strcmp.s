823051a6200850532bc063565eb225c5ecefd79f
Jolan <rathelotj@gmail.com>
2022-09-27 11:57:15 +0200

    ft_strcmp done

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -4,8 +4,31 @@ global _ft_strcmp
 _ft_strcmp:
 	PUSH	RBP ; establish a stack frame
     MOV		RBP, RSP
+	XOR		RAX, RAX ; i = 0
 
-	; PUSH	
-	; CALL	_ft_strlen ; calls 
+loop:
+	MOV		DL, BYTE [RSI + RAX]
+	CMP		BYTE [RDI + RAX], DL ; string1[i] == string2[i]
+	JL		strs_differ_less ; if string1[i] < string2[i]
+	JG		strs_differ_great ; if string1[i] > string2[i]  
+	CMP		BYTE [RDI + RAX], 0 ; if string1[i] == 0 and string1[i] == string2[i], then strings same
+	JE		strs_same ; if condition true, exit
+	INC		RAX ; i++
+	JMP		loop ; Continue Loop
+
+strs_differ_less:
+	XOR		RAX, RAX ; Set rax to negative 1
+	DEC		RAX
+	JMP		exit_prog
+
+strs_differ_great:
+	XOR		RAX, RAX ; Set rax to positive 1
+	INC		RAX
+	JMP		exit_prog
+
+strs_same:
+	XOR		RAX, RAX
+
+exit_prog:
 	LEAVE ; Tears down the stack frame
 	RET ; Returns RAX
--- srcs/tests/test.c
+++ srcs/tests/test.c
@@ -1,9 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrathelo <student.42nice.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 11:15:56 by jrathelo          #+#    #+#             */
+/*   Updated: 2022/09/27 11:55:47 by jrathelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdio.h>
 
 size_t ft_strlen(const char * string);
+int ft_strcmp(const char * s1, const char *s2);
 
 int main() {
 	printf("\"%s\" has length of %lu\n", "Hello ASM", ft_strlen("Hello ASM"));
 	printf("\"%s\" has length of %lu\n", "Hello ASM", strlen("Hello ASM"));
+
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "HELLO", ft_strcmp("HELLO", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "WORLD", ft_strcmp("HELLO", "WORLD"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "WORLD", "HELLO", ft_strcmp("WOLRD", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELL", "HELLO", ft_strcmp("HELL", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "HELL", ft_strcmp("HELLO", "HELL"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "HELLO", strcmp("HELLO", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "WORLD", strcmp("HELLO", "WORLD"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "WORLD", "HELLO", strcmp("WOLRD", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELL", "HELLO", strcmp("HELL", "HELLO"));
+	printf("\"%s\" is equal to \"%s\", %d\n", "HELLO", "HELL", strcmp("HELLO", "HELL"));
+	return (0);
 }
